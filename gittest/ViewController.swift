fecbacbecdbcfdc699da55a66bf5b7d137bdad4c
Florian Poncelin <fponcelin@me.com>
2016-01-10 18:44:29 +0100

    Initial Commit

---
+++ gittest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gittest
+//
+//  Created by Flori on 10/01/2016.
+//  Copyright © 2016 Florian Poncelin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
